5f28c762ca12f3990751dcb3b3d0e4bb1ef2183f
Anthony Minford <anthony.minford@deloitte.com>
2018-03-27 09:10:39 -0400

    Initial commit - created login page component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    this.navCtrl.push(LoginPage);
   }
 
 }
