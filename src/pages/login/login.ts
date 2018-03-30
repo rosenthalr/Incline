89e9f231045a3963b59c663a8b1be886e193ed5d
Anthony Minford <anthony.minford@deloitte.com>
2018-03-30 12:26:26 -0400

    Added conditional styling for input fields

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -17,11 +17,13 @@ export class LoginPage {
   public email: string;
   public password: string;
   public isComplete: boolean;
+  public emailError: boolean;
 
   constructor(private loginService: LoginService) {
     this.loginMessage = " ";
     this.active = false;
     this.isComplete = false;
+    this.emailError = false;
   }
 
   checkIfComplete(userInfo, field) {
@@ -42,6 +44,8 @@ export class LoginPage {
   }
 
   login(userInfo) {
+    console.log(userInfo);
+
       let user = {
         username: userInfo.email,
         password: userInfo.password
@@ -52,11 +56,12 @@ export class LoginPage {
 
         // log the success message to the console
         console.log(data);
-
+        this.emailError = false;
         // Not sure why I need to return true, but it doesn't work when I remove it
         return true;
       },
       error => {
+        this.emailError = true;
         console.error("Error logging in user!");
         return Observable.throw(error);
       }
