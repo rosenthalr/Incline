fc1fa9f466a9366a368e689ba9ac1bbfe9957d79
Adam <abujnowski@deloitte.com>
2018-04-29 10:34:25 -0400

    updated logos and styling

--- src/app/data/habits.ts
+++ src/app/data/habits.ts
@@ -3,25 +3,25 @@ export const workHabits = [
       "habit": "Get out of bed on your first alarm",
       "description": "The early bird gets the worm",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "alarm"
     },
     {
       "habit": "Visualize your next day",
       "description": "A surefire was to keep uncertainty at bay!",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "clipboard"
     },
     {
       "habit": "Pick 3 priorities for the day",
       "description": "Create some structure, discipline, and focus in your life",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "mail"
     },
     {
       "habit": "Check off your to-do list",
       "description": "Those tasks aren't going to check off themselves",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "hand"
     }
     
   ]
@@ -31,25 +31,25 @@ export const workHabits = [
       "habit": "Meditate",
       "description": "Take some time to focus on you.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "ios-search"
     },
     {
       "habit": "Go to sleep without a device",
       "description": "Unplug and unwind.",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "phone-portrait"
     },
     {
-      "habit": "get 8 hours of sleep",
+      "habit": "Get 8 hours of sleep",
       "description": "Kick back and drift into dream world.",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "moon"
     },
     {
       "habit": "Read before going to bed",
       "description": "Distract your mind from those daily stresses.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "bookmarks"
     }
     
   ]
@@ -59,25 +59,25 @@ export const workHabits = [
       "habit": "Drink 64 ounces of water",
       "description": "Motivate, hydrate feel great.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "water"
     },
     {
       "habit": "Do 30 minutes of exercise",
       "description": "No matter how slow you go, you're still lapping everyone on the couch.",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "flash"
     },
     {
       "habit": "Stretch for 20 minutes",
       "description": "Keep those muscles flexible, strong, and healthy for the road ahead.",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "move"
     },
     {
       "habit": "Eat vegetables with dinner",
       "description": "Healthy is not a size, it's a lifestyle.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "pizza"
     }
     
   ]
@@ -88,25 +88,25 @@ export const workHabits = [
       "habit": "Eat breakfast",
       "description": "give yourself the fuel to start firing on all cylinders.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "pizza"
     },
     {
       "habit": "Read the news",
       "description": "#BlessThePress",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "glasses"
     },
     {
       "habit": "Make your bed",
       "description": "Accomplish that first task and set yourself up for success.",
       "reminder": 9,
-      "logo": "placeholder"
+      icon: "checkmark"
     },
     {
       "habit": "Take your vitamins",
       "description": "Your insurance policy to getting the nutrients you need.",
       "reminder": 7,
-      "logo": "placeholder"
+      icon: "medkit"
     }
     
   ]
