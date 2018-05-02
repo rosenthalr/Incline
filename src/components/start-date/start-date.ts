5a6e9283d83e07beef02da9e315fa8f7efb4b297
Anthony Minford <anthony.minford@deloitte.com>
2018-05-02 12:34:06 -0400

    Added styling to start-date component

--- src/components/start-date/start-date.ts
+++ src/components/start-date/start-date.ts
@@ -15,15 +15,21 @@ export class StartDateComponent {
   @Output() goBack = new EventEmitter<any>();
   @Output() nextPage: EventEmitter<any> = new EventEmitter<any>();
   startDate: string;
-  targetDate: moment.Moment;
+  targetDate: string;
+  min: string;
+  max: string;
+  hasChanged: boolean = false;
 
   constructor() {
     this.startDate = moment().day(2).toISOString();
-    this.targetDate = moment().add(22,'days');
+    this.min = moment().day(2).toISOString();
+    this.max = moment(this.min).add(5, 'years').toISOString();
+    this.targetDate = moment(this.startDate).add(21,'days').toISOString();
   }
 
   setTargetDate() {
-    this.targetDate = moment(this.startDate).add(22,'days');
+    this.targetDate = moment(this.startDate).add(21,'days').toISOString();
+    this.hasChanged = true;
   }
 
   emitGoBack() {
