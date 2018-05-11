fadcdb2d4726d6b59298955038d4f2837300067d
immacmillan <immacmillan@gmail.com>
2018-05-11 16:32:00 -0400

    added reset validation  condition: can't reset to current password

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -45,19 +45,21 @@ export class MorePage implements OnInit {
   public password3: string;
   public passwordError: boolean;
   public confirmError: boolean;
+  public sameError: boolean;
   public confirmComplete: boolean;
   public more: More;
   public active: boolean;
   public f: FormGroup;
-  
+
 
   constructor(private toastCtrl: ToastController, private moreResetService: MoreResetService, private actionSheetCtrl: ActionSheetController,
     private logoutService: LogoutService, private navCtrl: NavController, private navParams: NavParams) {
     this.isComplete = false;
     this.passwordError = false;
     this.active = false;
-    this.confirmError = false; 
-    this.confirmComplete=false;
+    this.confirmError = false;
+    this.sameError = false;
+    this.confirmComplete = false;
   }
   ngOnInit() {
     this.more = {
@@ -68,8 +70,8 @@ export class MorePage implements OnInit {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MorePage');
-    console.log('pw   '+localStorage.getItem("pw") +localStorage.getItem("pw").length)
-    console.log('passwordError   '+ this.passwordError )
+    console.log('pw   ' + localStorage.getItem("pw") + localStorage.getItem("pw").length)
+    console.log('passwordError   ' + this.passwordError)
   }
   showPassword() {
     this.showPass = !this.showPass;
@@ -111,55 +113,63 @@ export class MorePage implements OnInit {
 
   checkIfComplete(userInfo, field) {
 
-    if (field=='password') {
+    if (field == 'password') {
       this.password = userInfo;
-      this.isComplete=true;
+      this.isComplete = true;
     } else if (field == 'password2') {
-      this.password2= userInfo;
+      this.password2 = userInfo;
     }
-    else if (field =='password3'){
+    else if (field == 'password3') {
       this.password3 = userInfo;
-      this.confirmComplete=true;
-    }
-  
-  }
-  
-  checkIfConfirm(userInfo, field) { 
-  if(this.confirmComplete=true){
-    if(this.password3.length > this.password2.length-1){
-        console.log(this.active);
-      if (this.password2!==this.password3) {
-        this.confirmError=true;
-        this.active=false;
-      }else {
-        this.confirmError=false;
-        this.active = true;
-      }
-    }
+      this.confirmComplete = true;
     }
   }
 
-  checkIfCorrect(userInfo, field) { 
-    if(this.isComplete=true){
-      if(this.password.length > localStorage.getItem("pw").length-1){
-        // console.log('isComplete:   ' + this.isComplete)
-        // console.log('passwordError:    ' + this.passwordError + '     pw ----- '+ this.password + '  l  '+ this.password.length)
-        if(this.password!==localStorage.getItem("pw")){
-          this.passwordError = true;      
-            if(field=='password') {
-              this.password = userInfo;
-            } 
-          } else
-          {
-            this.passwordError = false;
-            this.active = false;
-          } 
-        } 
+  checkIfSame(userInfo, field) {
+    if (this.password2 !== localStorage.getItem("pw")) {
+      this.sameError = false;
+    } else {
+      this.sameError = true;
+      this.active = false;
+    }
+  }
+
+  checkIfConfirm(userInfo, field) {
+    if (this.confirmComplete = true) {
+      if (this.password3.length > this.password2.length - 1) {
+        console.log(this.active);
+        if(this.password2 !== localStorage.getItem("pw")) {
+        if (this.password2 !== this.password3 ) {
+          this.confirmError = true;
+          this.active = false;
+        } else {
+          this.confirmError = false;
+          this.active = true;
         }
       }
+    }
+  }
+  }
+  checkIfCorrect(userInfo, field) {
+    if (this.isComplete = true) {
+      if (this.password.length > localStorage.getItem("pw").length - 1) {
+        // console.log('isComplete:   ' + this.isComplete)
+        // console.log('passwordError:    ' + this.passwordError + '     pw ----- '+ this.password + '  l  '+ this.password.length)
+        if (this.password !== localStorage.getItem("pw")) {
+          this.passwordError = true;
+          if (field == 'password') {
+            this.password = userInfo;
+          }
+        } else {
+          this.passwordError = false;
+          this.active = false;
+        }
+      }
+    }
+  }
 
-      
-    
+
+
   logout(user) {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Are you sure you want to log out?',
@@ -202,7 +212,6 @@ export class MorePage implements OnInit {
       password2: userInfo.password2,
       password3: userInfo.password3
     };
-
     this.moreResetService.resetuser(user).subscribe(
       data => {
         let toast = this.toastCtrl.create({
