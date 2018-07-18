21819b4d32ba54b7a46446cd390bc7c88f5f8bf3
Adam <abujnowski@deloitte.com>
2018-07-18 18:58:55 -0400

    3 viz fixes

--- src/services/habitdelete.service.ts
+++ src/services/habitdelete.service.ts
@@ -7,12 +7,12 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class HabitDeleteService {
 
-  public apiUrl = 'https://ec2-35-168-103-149.compute-1.amazonaws.com/habitaction';
+  // public apiUrl = 'https://ec2-35-168-103-149.compute-1.amazonaws.com/habitaction';
   // public apiUrl = 'https://goals.digitalstudio.io/habitaction';
   
 
 
-  //public apiUrl = 'http://localhost:7000/habitaction';
+  public apiUrl = 'http://localhost:7000/habitaction';
 
   // Inject HttpClient module into service so can make Rest API calls
   constructor(public http: HttpClient) { }
